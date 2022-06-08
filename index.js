2c049a747025b3ec46662e16238a06104279fcc6
Chris Hendricks <crshenx@gmail.com>
2022-06-08 14:37:09 -0700

    commit tree 3633f716008506f5980ee49c74ea41499dd2cd80

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(array, string) {
+  return array.filter((name) => name.toLowerCase() === string.toLowerCase());
+}
+
+function fuzzyMatch(array, string) {
+  return array.filter((name) => name[0] === string[0]);
+}
+
+function matchName(array, string) {
+  return array.filter((obj) => obj.name === string);
+}
